ca67406c0bfd19ec550a941a55788b49424aa490
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2023-01-08 12:38:11 +0100

    initial commit

---
+++ Assets/Scripts/RotateController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateController : MonoBehaviour
+{
+    [SerializeField]
+    private float rotation_speed = 10.0f;
+
+    public Camera cam;
+
+    private void OnMouseDrag()
+    {
+        float x_rotation = Input.GetAxis("Mouse X") * rotation_speed;
+        float y_rotation = Input.GetAxis("Mouse Y") * rotation_speed;
+
+        Vector3 right = Vector3.Cross(cam.transform.up, transform.position - cam.transform.position);
+        //Vector3 up = 
+    }
+}
